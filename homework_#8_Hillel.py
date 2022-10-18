6c3063a5be908fe6be611610f5c84b5966ad59ac
gavrylychev99 <gavrylychev99@gmail.com>
2022-10-18 18:34:09 +0300

    Homework Hillel lesson#8 (no full)

--- homework_#8_Hillel.py
+++ homework_#8_Hillel.py
@@ -2,29 +2,3 @@ from Mode_for_Homework import new_list
 print(new_list)
 
 # Homework #2
-
-# my_list1 = []
-# for element in my_list:
-#     if element == my_list[1::2]:
-#         my_list1 = my_list[::-1]
-#     elif element == my_list[::2]:
-#         my_list1 = element
-# return my_list1
-# def create_point(x: float = 1, y: float = 10, z: float = 9):
-#     point = {'x': x,
-#              'y': y}
-#     return point
-#
-#
-# p = create_point(3, 4)
-# print(p)
-#
-#
-# def where_is_point(point: dict):
-#     if point['x'] < 0 and point['y'] > 0:
-#         return 1
-#     else:
-#         return 2
-#
-#
-# print(where_is_point(p))
