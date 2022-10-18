30144ed0b6a9ba921c505848be9e8c24d8e0c8eb
gavrylychev99 <gavrylychev99@gmail.com>
2022-10-18 18:37:24 +0300

    Revert "Delete Mode_for_Homework.py"
    This reverts commit 2535a4a82e1fb7c16a5a83633c9fb103d26930e3.

---
+++ Mode_for_Homework.py
@@ -0,0 +1,25 @@
+# Homework 1
+def set1(a: str = 'Valeriy', b: str = 'Vova', c: str = 'Sergey', d: str = 'Pasha'):
+    my_list = [a] + [b[::-1]] + [c] + [d[::-1]]
+    print(type(my_list))
+    return my_list
+
+
+new_list = set1
+
+
+# Homework 2
+# Написать функцию которой передается один параметр - список строк my_list.
+# Функция возвращает новый список в котором содержаться
+# элементы из my_list у которых первый символ - буква "a".
+
+def set2(a: str = 'Valeriy', b: str = 'Vova', c: str = 'Sergey', d: str = 'Pasha', s: str = 'Aleksey'):
+    my_list2 = [a, b, c, d]
+    for element in:
+        if element in my_list2:
+            my_list2 = element.find('a')
+    return my_list2
+
+
+new_list2 = set2()
+print(new_list2)
