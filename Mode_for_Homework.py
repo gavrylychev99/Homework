900e61eeeaa9329de094b887c44db99769b8c051
gavrylychev99 <gavrylychev99@gmail.com>
2022-10-21 17:27:52 +0300

    mode #1 Hillel homework#8

--- Mode_for_Homework.py
+++ Mode_for_Homework.py
@@ -1,23 +1,42 @@
 # Homework #1
-def set1(a: str = 'Valeriy', b: str = 'Vova', c: str = 'Sergey', d: str = 'Pasha'):
-    my_list = [a] + [b[::-1]] + [c] + [d[::-1]]
-    return my_list
+def set1(my_list: list):
+    new_list = []
+    for index in range(len(my_list)):
+        if index % 2 != 0:
+            new_list.append(my_list[index][::-1])
+        else:
+            new_list.append(my_list[index])
+    return new_list
 
 
-new_list = set1()
+li = ['Valeriy', 'Vova', 'Sergey', 'Pasha', 'Kirill']
+print(set1(li))
+
 
 # Homework 2
+def set2(my_list: list):
+    new_list = []
+    for element in my_list:
+        if element[0] == 'A':
+            new_list.append(element)
+    return new_list
+
+
+li2 = ['Alexandr', 'Vova', 'Sergey', 'Pasha', 'Kirill']
+print(set2(li2))
+
+
+# Homework 3
 # Написать функцию которой передается один параметр - список строк my_list.
 # Функция возвращает новый список в котором содержаться
-# элементы из my_list у которых первый символ - буква "a".
+# элементы из my_list в которых есть символ - буква "a" на любом месте.
 
-# def set2(a: str = 'Valeriy', b: str = 'Vova', c: str = 'Sergey', d: str = 'Pasha', s: str = 'Aleksey'):
-#     my_list2 = [a, b, c, d]
-#     for element in:
-#         if element in my_list2:
-#             my_list2 = element.find('a')
-#     return my_list2
-#
-#
-# new_list2 = set2()
-# print(new_list2)
+def set3(my_list: list):
+    new_list = []
+    for element in my_list:
+        element.find('a')
+    return new_list
+
+
+li3 = ['Valeriy', 'Vova', 'Sergey', 'Pasha', 'Kirill']
+print(set3(li3))
--- homework_#8_Hillel.py
+++ homework_#8_Hillel.py
@@ -1,5 +1,7 @@
 # Homework #1
-from Mode_for_Homework import new_list
-print(new_list)
+from Mode_for_Homework import set1
 
 # Homework #2
+from Mode_for_Homework import set2
+
+# Homework #3
