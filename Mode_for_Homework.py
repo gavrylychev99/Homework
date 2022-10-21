5a78373db05faf641dcd2205bb906fd603a53971
gavrylychev99 <gavrylychev99@gmail.com>
2022-10-21 21:41:27 +0300

    mode_for_homework #8

--- Mode_for_Homework.py
+++ Mode_for_Homework.py
@@ -27,16 +27,15 @@ print(set2(li2))
 
 
 # Homework 3
-# Написать функцию которой передается один параметр - список строк my_list.
-# Функция возвращает новый список в котором содержаться
-# элементы из my_list в которых есть символ - буква "a" на любом месте.
-
 def set3(my_list: list):
     new_list = []
     for element in my_list:
-        element.find('a')
+        if element.find('a') != -1:
+            new_list.append(element)
     return new_list
 
 
 li3 = ['Valeriy', 'Vova', 'Sergey', 'Pasha', 'Kirill']
 print(set3(li3))
+
+# Homework 4
